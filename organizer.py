138d7c13146ce9e5fd94ede4ae92568e021e578c
Harin Lee <harinlee83@gmail.com>
2022-03-09 20:49:25 -0500

    Added component 2 organizer and TermMapping.csv

--- organizer.py
+++ organizer.py
@@ -1,12 +1,16 @@
+# Component 1: Therefore to appropriately organize the data, for each dataset with semicolon denoted subsets in Column I we
+# should create an additional row with the same metadata for all fields except Column I, and having one of the 
+# unique terms broken up by semicolons in Column I in each row.
+
 import csv
 
 # Insert "consent title" column letter (captialized) here
-row_letter = "I"
-CONSENT_TITLE_ROW_NUMBER = ord(row_letter) - ord("A")
+consent_title_column_letter = "I"
+CONSENT_TITLE_COLUMN_NUMBER = ord(consent_title_column_letter) - ord("A")
 
 # Insert csv file names here
 original_CSV_file = "DUO Validation Project - Development Dataset - Sheet1.csv"
-new_CSV_file = "ORGANIZED: DUO Validation Project - Development Dataset - Sheet1.csv"
+new_CSV_file = "ORGANIZED_v1: DUO Validation Project - Development Dataset - Sheet1.csv"
 
 with open(original_CSV_file, "r") as originalFile:
     with open(new_CSV_file, "w") as newFile:
@@ -20,14 +24,14 @@ with open(original_CSV_file, "r") as originalFile:
 
         for line in reader:
             # Check to see if there are multiple subsets in "consent title" column
-            if len(line[CONSENT_TITLE_ROW_NUMBER]) > 1:
+            if len(line[CONSENT_TITLE_COLUMN_NUMBER]) > 1:
                 # Parse each subset into an element of an array
-                Titles = line[CONSENT_TITLE_ROW_NUMBER].split(";")
+                Titles = line[CONSENT_TITLE_COLUMN_NUMBER].split(";")
                 numTitles = len(Titles)
                 for extraRow in range(numTitles):
                     # Make a changeable copy of current line
                     copyLine = line.copy()
-                    copyLine[CONSENT_TITLE_ROW_NUMBER] = Titles[extraRow].strip()
+                    copyLine[CONSENT_TITLE_COLUMN_NUMBER] = Titles[extraRow].strip()
                     writer.writerow(copyLine)
             else:
                 writer.writerow(line)
---
+++ organizer2.py
@@ -0,0 +1,75 @@
+# Component 2: Build a script which can parse Column I and search for a list of terms (in TermMapping.csv)),
+# and when a term or terms are identified, add a corresponding permanent Data Use Ontology URL (PURL) for each
+# in a new column, Column J. This may involve adding multiple PURLs to a column.
+
+# This code builds off of code from organizer.py
+
+import csv
+
+# Insert "consent title" column letter (captialized) here
+consent_title_column_letter = "I"
+CONSENT_TITLE_COLUMN_NUMBER = ord(consent_title_column_letter) - ord("A")
+
+# Insert "PURL" column letter (captialized) here
+PURL_title_column_letter = "J"
+name_of_PURL_column = "PURLs"
+PURL_COLUMN_NUMBER = ord(PURL_title_column_letter) - ord("A")
+
+# Insert csv file names here
+original_CSV_file = "ORGANIZED_v1: DUO Validation Project - Development Dataset - Sheet1.csv"
+new_CSV_file = "ORGANIZED_v2: DUO Validation Project - Development Dataset - Sheet1.csv"
+list_of_terms_CSV_file = "TermMapping.csv"
+
+# Converts the TermMapping CSV file into a dict called mappedData to query through.
+# Key = Subset Consent Title, Value = PURLs
+mappedData = {}
+with open(list_of_terms_CSV_file, "r") as mappingFile:
+    mappedReader = csv.DictReader(mappingFile)
+    for line in mappedReader:
+        mappedData.setdefault(line["Query for:"],[]).append(line["If found, then add*:"].strip())
+
+# Create dict like mappedData where all keys are uppercased
+upperMappedData = {}
+for key in mappedData:
+    upperMappedData[key.upper()] = mappedData[key]
+
+with open(original_CSV_file, "r") as originalFile:
+    with open(new_CSV_file, "w") as newFile:
+
+        reader = csv.reader(originalFile)
+        writer = csv.writer(newFile)
+
+        # Store the column headers, add a new PURL column, print them in newFile
+        columnHeaders = next(reader)
+        columnHeaders.insert(PURL_COLUMN_NUMBER,name_of_PURL_column)
+        writer.writerow(columnHeaders)
+
+        for line in reader:
+            # Make a changeable copy of current line
+            copyLine = line.copy()
+            # Insert empty cell
+            copyLine.insert(PURL_COLUMN_NUMBER,'')
+            # Look for key match where capitalization variations are accounted for
+            if copyLine[CONSENT_TITLE_COLUMN_NUMBER].upper() in upperMappedData:
+                # Find subset key consent title in list of terms and insert extra cell in current line for PURL column
+                key = copyLine[CONSENT_TITLE_COLUMN_NUMBER].upper()
+                purl = str(upperMappedData[key]).strip("['']")
+                # Strip '[]' in the URL so it becomes a clickable link
+                copyLine[PURL_COLUMN_NUMBER] = purl
+            else:
+                # Fragment consent_title by whitespace into list of subsets
+                subsets = line[CONSENT_TITLE_COLUMN_NUMBER].split(" ")
+                # Strip () and , from the subsets
+                for subset in range(len(subsets)):
+                    subsets[subset] = subsets[subset].replace("("," ")
+                    subsets[subset] = subsets[subset].replace(")"," ")
+                    subsets[subset] = subsets[subset].replace(","," ")
+                    subsets[subset] = subsets[subset].strip()
+                    subsets[subset] = subsets[subset].strip().upper()
+                for key in subsets:
+                    if key in upperMappedData:
+                        purl = str(upperMappedData[key]).strip("['']") + '\n'
+                        copyLine[PURL_COLUMN_NUMBER] = copyLine[PURL_COLUMN_NUMBER] + purl
+
+            copyLine[PURL_COLUMN_NUMBER] = copyLine[PURL_COLUMN_NUMBER].strip()
+            writer.writerow(copyLine)
