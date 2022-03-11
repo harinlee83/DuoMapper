92f0c58712a6765db3709bbbcf868e723cd93b3c
Harin Lee <harinlee83@gmail.com>
2022-03-10 23:14:50 -0500

    Simplified code with regex

--- organizer2.py
+++ organizer2.py
@@ -10,14 +10,18 @@
 # Should "Disease Specific" include any consent title that contains the word "disease" in it?
 
 import csv
+import re
 
 # Insert "consent title" column letter (captialized) here
-consent_title_column_letter = input("Insert consent title column letter (capitalized): ")
+# consent_title_column_letter = input("Insert consent title column letter (capitalized): ")
+consent_title_column_letter = 'C'
 CONSENT_TITLE_COLUMN_NUMBER = ord(consent_title_column_letter) - ord("A")
 
 # Insert "PURL" column letter (captialized) here
-PURL_title_column_letter = input("Insert column letter (capitalized) for PURLs: ")
-name_of_PURL_column = input("Insert name of PURLs column: ")
+# PURL_title_column_letter = input("Insert column letter (capitalized) for PURLs: ")
+PURL_title_column_letter = 'D'
+# name_of_PURL_column = input("Insert name of PURLs column: ")
+name_of_PURL_column = "PURLs"
 PURL_COLUMN_NUMBER = ord(PURL_title_column_letter) - ord("A")
 
 # Insert csv file names here
@@ -55,69 +59,14 @@ with open(original_CSV_file, "r") as originalFile:
             # Insert empty cell
             copyLine.insert(PURL_COLUMN_NUMBER,'')
             # Look for key match where capitalization variations are accounted for
-            if copyLine[CONSENT_TITLE_COLUMN_NUMBER].upper() in upperMappedData or copyLine[CONSENT_TITLE_COLUMN_NUMBER].upper() + " " in upperMappedData:
-                # Find subset key consent title in list of terms and insert extra cell in current line for PURL column
-                key = copyLine[CONSENT_TITLE_COLUMN_NUMBER].upper()
-                # Strip '[]' in the URL so it becomes a clickable link
-                purl = str(upperMappedData[key]).strip("['']")
-                copyLine[PURL_COLUMN_NUMBER] = purl
-            # This elif is used to differentiate between General Research Use () and Disease-Specific ()
-            elif "(" and "/" in copyLine[CONSENT_TITLE_COLUMN_NUMBER]:
-                # Fragment consent_title by left parenthesis, comma, and white space into list of subsets
-                subsets = line[CONSENT_TITLE_COLUMN_NUMBER].replace("(",",").replace(" and ",",").replace(" ",",").split(",")
-                # Strip ) and , from the subsets
-                for subset in range(len(subsets)):
-                    subsets[subset] = subsets[subset].replace(")"," ")
-                    subsets[subset] = subsets[subset].strip()
-                    subsets[subset] = subsets[subset].strip().upper()
-                for key in subsets:
-                    if key in upperMappedData:
-                        purl = str(upperMappedData[key]).strip("['']") + '\n'
+            for key in upperMappedData:
+                pattern = re.compile(r'\b' + key + r'\b')
+                matches = re.search(pattern,copyLine[CONSENT_TITLE_COLUMN_NUMBER].upper())
+                # If query term is found
+                if matches:
+                    # Strip '[]' in the URL so it becomes a clickable link
+                    purl = str(upperMappedData[key]).strip("['']") + '\n'
+                    if purl not in copyLine[PURL_COLUMN_NUMBER]:
                         copyLine[PURL_COLUMN_NUMBER] = copyLine[PURL_COLUMN_NUMBER] + purl
-            elif "(" in copyLine[CONSENT_TITLE_COLUMN_NUMBER]:
-                # Fragment consent_title by left parenthesis, comma into list of subsets
-                subsets = line[CONSENT_TITLE_COLUMN_NUMBER].upper().replace("(",",").replace(" and ",",").split(",")
-                # Strip ")", "USE", "ONLY"  and "," from the subsets
-                for subset in range(len(subsets)):
-                    subsets[subset] = subsets[subset].replace(")"," ")
-                    subsets[subset] = subsets[subset].replace("USE","")
-                    subsets[subset] = subsets[subset].replace("ONLY","")
-                    subsets[subset] = subsets[subset].strip()
-                for key in subsets:
-                    if key in upperMappedData:
-                        purl = str(upperMappedData[key]).strip("['']") + '\n'
-                        copyLine[PURL_COLUMN_NUMBER] = copyLine[PURL_COLUMN_NUMBER] + purl
-            elif "FOR" in copyLine[CONSENT_TITLE_COLUMN_NUMBER].upper():
-                # Fragment consent_title by left parenthesis, comma into list of subsets
-                subsets = line[CONSENT_TITLE_COLUMN_NUMBER].upper().split(",")
-                # Remove "FOR", "USE", "ONLY" from the subsets
-                for subset in range(len(subsets)):
-                    subsets[subset] = subsets[subset].replace("FOR ","")
-                    subsets[subset] = subsets[subset].replace("ONLY","")
-                    subsets[subset] = subsets[subset].strip()
-                for key in subsets:
-                    if key in upperMappedData:
-                        purl = str(upperMappedData[key]).strip("['']") + '\n'
-                        copyLine[PURL_COLUMN_NUMBER] = copyLine[PURL_COLUMN_NUMBER] + purl
-            elif "-" in copyLine[CONSENT_TITLE_COLUMN_NUMBER].upper():
-                # Fragment consent_title by left parenthesis, comma into list of subsets
-                subsets = line[CONSENT_TITLE_COLUMN_NUMBER].upper().split(" ")
-                for key in subsets:
-                    if key in upperMappedData:
-                        purl = str(upperMappedData[key]).strip("['']") + '\n'
-                        copyLine[PURL_COLUMN_NUMBER] = copyLine[PURL_COLUMN_NUMBER] + purl
-            else:
-                # Fragment consent_title by "ON", "AND", "WITH" and "," and - into list of subsets.
-                subsets = line[CONSENT_TITLE_COLUMN_NUMBER].upper().replace(" AND ","-").replace(" ON ","-").replace(" WITH ","-").replace(",","-").split("-")
-                # Strip whitespace, "USE" and "ONLY" from the subsets
-                for subset in range(len(subsets)):
-                    subsets[subset] = subsets[subset].replace("USE","")
-                    subsets[subset] = subsets[subset].replace("ONLY","")
-                    subsets[subset] = subsets[subset].strip()
-                for key in subsets:
-                    if key in upperMappedData:
-                        purl = str(upperMappedData[key]).strip("['']") + '\n'
-                        copyLine[PURL_COLUMN_NUMBER] = copyLine[PURL_COLUMN_NUMBER] + purl
-
             copyLine[PURL_COLUMN_NUMBER] = copyLine[PURL_COLUMN_NUMBER].strip()
             writer.writerow(copyLine)
