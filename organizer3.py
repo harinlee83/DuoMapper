b116036fce125f73918718e373d00bc54fe717dd
Harin Lee <harinlee83@gmail.com>
2022-04-16 02:39:23 -0400

    First attempt at organizer3.py. It works but a little slow

--- organizer3.py
+++ organizer3.py
@@ -3,10 +3,70 @@
 # multiple PURLs to a column.
 
 # This code builds off of code from organizer2.py
+# This code uses BeautifulSoup and Requests for "web scraping" a dynamic JavaScript Website
 
 import requests
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+import json
 
-url = 'https://www.ebi.ac.uk/ols/search?q=cancer&groupField=iri&start=0&ontology=mondo&ontology=doid'
-r = requests.get(url)
+def get_JSON(url):
+    response = requests.get(url)
+    if not response.ok:
+        print(f'Code: {response.status_code}, url: {url}')
+    return response.text
 
-print(r.text)
+def get_Purl(queryTerm,yesDOID):
+    baseURL = "https://www.ebi.ac.uk/ols/api/search?q="
+    iriURL = "&groupField=iri"
+    # This refers to page numbers
+    start = 0
+    pageIndexURL = f"&start={start}"
+
+    doidURL = "&ontology=doid"
+    mondoURL = "&ontology=mondo"
+
+    if yesDOID == True:
+        url = baseURL + queryTerm + iriURL + pageIndexURL + doidURL
+    else:
+        url = baseURL + queryTerm + iriURL + pageIndexURL + mondoURL
+
+    json_Text = get_JSON(url)
+    my_json = json.loads(json_Text)
+    # This finds the total number of results
+    maxResultNum = my_json["response"]["numFound"]
+    start = my_json["response"]["start"]
+    my_purls = []
+    for label in my_json["response"]["docs"]:
+            if label["iri"] not in my_purls:
+                my_purls.append(label["iri"])
+
+    while start < maxResultNum:
+        start += 10
+        pageIndexURL = f"&start={start}"
+        url = baseURL + queryTerm + iriURL + pageIndexURL + doidURL
+        json_Text = get_JSON(url)
+        my_json = json.loads(json_Text)
+
+        for label in my_json["response"]["docs"]:
+            if label["iri"] not in my_purls:
+                my_purls.append(label["iri"])
+    return my_purls
+
+def main():
+    queryTerm = "ADHD"
+    # This ensures that the query term is URL encoded
+    queryTerm = quote(queryTerm)
+    doidPurls = get_Purl(queryTerm, True)
+    mondoPurls = get_Purl(queryTerm, False)
+
+    print("MONDO Purls")
+    for purl in mondoPurls:
+        print(purl)
+
+    print("DOID Purls")
+    for purl in doidPurls:
+        print(purl)
+
+if __name__ == "__main__":
+    main()
